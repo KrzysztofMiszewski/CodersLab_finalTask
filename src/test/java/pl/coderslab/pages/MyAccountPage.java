71c53d7b0a15b8a7266838339dc9569fc7cecb8a
KrzysztofMiszewski <krzysztofxmiszewski@gmail.com>
2021-12-29 14:40:41 +0100

    NewAddressPage

--- src/test/java/pl/coderslab/pages/MyAccountPage.java
+++ src/test/java/pl/coderslab/pages/MyAccountPage.java
@@ -1,4 +1,20 @@
 package pl.coderslab.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class MyAccountPage {
+
+    private final WebElement addressButon;
+
+
+    public MyAccountPage(WebDriver driver) {
+
+        this.addressButon = driver.findElement(By.id("address-link"));
+    }
+
+    public void ClickAddressButton() {
+        addressButon.click();
+    }
 }
---
+++ src/test/java/pl/coderslab/pages/NewAddressPage.java
@@ -0,0 +1,43 @@
+package pl.coderslab.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class NewAddressPage {
+
+
+    private final WebElement aliasField;
+    private final WebElement addressField;
+    private final WebElement postalField;
+    private final WebElement cityField;
+    private final Select countrySelect;
+    private final WebElement phoneField;
+    private final WebElement saveButton;
+
+
+    public NewAddressPage(WebDriver driver) {
+
+        this.aliasField = driver.findElement(By.cssSelector("div.form-group:nth-child(5) > div:nth-child(2) > input:nth-child(1)"));
+        this.addressField = driver.findElement(By.cssSelector("div.form-group:nth-child(10) > div:nth-child(2) > input:nth-child(1)"));
+        this.postalField = driver.findElement(By.cssSelector("div.form-group:nth-child(12) > div:nth-child(2) > input:nth-child(1)"));
+        this.cityField = driver.findElement(By.cssSelector("div.form-group:nth-child(13) > div:nth-child(2) > input:nth-child(1)"));
+        this.countrySelect = new Select(driver.findElement(By.cssSelector("select.form-control")));
+        this.phoneField = driver.findElement(By.cssSelector("div.form-group:nth-child(15) > div:nth-child(2) > input:nth-child(1)"));
+        this.saveButton = driver.findElement(By.cssSelector("button.btn"));
+    }
+
+    public void AddAddress(String alias, String address, String postal, String city, String country, String phone) {
+
+        aliasField.sendKeys(alias);
+        addressField.sendKeys(address);
+        postalField.sendKeys(postal);
+        cityField.sendKeys(city);
+        countrySelect.selectByVisibleText(country);
+        phoneField.sendKeys(phone);
+        saveButton.click();
+
+    }
+
+}
